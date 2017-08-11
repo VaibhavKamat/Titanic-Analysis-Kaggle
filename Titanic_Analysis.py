38cf31d7af53d39dfa0981c980744c12f8d23c49
Vaibhav Kamat <vaibhav_kamat@persistent.co.in>
2017-08-11 16:51:31 +0530

    Add Bar chart for people grouped by Pclass

--- Titanic_Analysis.py
+++ Titanic_Analysis.py
@@ -12,57 +12,91 @@ import unicodecsv
 from collections import defaultdict
 
 def percentage(arr):
-    '''Returns the percentage of a particular attribute in a dataset
-    '''
+    '''Returns the percentage of a particular attribute in a dataset'''
     return np.mean(arr)*100
 
 def get_crew_data(data):
-    '''Returns the total number of crew members on board and number of crew members survived.
-    '''
-    c,s=0,0
+    '''Returns the total number of crew members on board and number of crew members survived.'''
+    c,s = 0,0
     for i in range(len(data)):
         
-        if str(data.iloc[i]['Cabin'])!='nan':
+        if str(data.iloc[i]['Cabin']) != 'nan':
             c+=1
-            if data.iloc[i]['Survived']==1:
+            if data.iloc[i]['Survived'] == 1:
                 s+=1
     return c,s
     
-def autolabel(rects):
+def autolabel(rects,x=0.15,y=0.6):
     '''Labels the bar graphs according to the height of the bars
     '''
     for rect in rects:
-        h=rect.get_height()
-        plt.text(rect.get_x()+0.15,0.6*h,int(h),ha='center',va='bottom',color='white',size=12)
+        h = rect.get_height()
+        plt.text(rect.get_x()+x,y*h,int(h),ha='center',va='bottom',color='white',size=12)
     
-    
-        
-data = pd.read_csv(r"E:\Udacity\Data Analysis\Project\titanic_data.csv")
-data.Age[data.Pclass == 1].plot(kind='kde')    
-data.Age[data.Pclass == 2].plot(kind='kde')
-data.Age[data.Pclass == 3].plot(kind='kde')
+data = pd.read_csv("titanic_data.csv")
+  
+
+# Age distribution within the passenger classes (Kernel Density Estimation)     
+ax = plt.subplot2grid((2,3),(0,0), colspan=3)
+data.Age[data.Pclass == 1].plot(kind='kde',color='blue')    
+data.Age[data.Pclass == 2].plot(kind='kde',color='red')
+data.Age[data.Pclass == 3].plot(kind='kde',color='orange')
 plt.xlabel("Age")    
 plt.title("Age Distribution (Kernel Density Estimation) Within Classes")
 plt.legend(('1st Class', '2nd Class','3rd Class')) 
 plt.show()
 
 
+
+# Number of males and females onboard the Titanic, grouped according to the passenger class
+data_by_pclass = data.groupby(['Pclass','Sex'])
+males_onboard = []
+females_onboard = []
+
+for i in range(1,4):
+    females_onboard.append(data_by_pclass['Survived'].count()[i][0])
+    males_onboard.append(data_by_pclass['Survived'].count()[i][1])
+    
+width = 0.3
+index = np.arange(3)
+fig,ax = plt.subplots()
+   
+ax = plt.subplot2grid((2,3),(1,0), colspan=3)   
+rects1 = ax.bar(index,females_onboard,width,color='blue')
+rects2 = ax.bar(index+width,males_onboard,width,color='green')      
+ax.set_xticks(index+width)
+ax.set_xticklabels(('Class1','Class2','Class3'))
+ax.legend((rects1[0],rects2[0]),('Females','Males'),loc='best')
+ax.set_ylim(0,400)
+plt.title("Number of males and females onboard the Titanic, grouped according to the passenger class")
+autolabel(rects1,0.15,0.2)
+autolabel(rects2,0.15,0.2)
+plt.show()
+
+
+
+
+
+
+
 #############################################################################################
-total_survived=(data['Survived']).sum()
+
+# Number Of People And Those Survived, Grouped By The Passenger Class
+total_survived = (data['Survived']).sum()
 print "Number of people survived are:%d"%(total_survived)
 
-data_by_pclass=data.groupby('Pclass')
+data_by_pclass = data.groupby('Pclass')
 total = data_by_pclass['Survived'].count()
 survived = data_by_pclass['Survived'].sum()
 not_survived = total-survived
 
-width=0.3
-index=np.arange(3)
-fig,ax=plt.subplots()
-colour=[]
-rects1=ax.bar(index,total,width,color='blue')
-rects2=ax.bar(index+width,survived,width,color='green')
-rects3=ax.bar(index+2*width,not_survived,width,color='red')
+width = 0.3
+index = np.arange(3)
+fig,ax = plt.subplots()
+
+rects1 = ax.bar(index,total,width,color='blue')
+rects2 = ax.bar(index+width,survived,width,color='green')
+rects3 = ax.bar(index+2*width,not_survived,width,color='red')
 ax.set_xticks(index+width)
 ax.set_xticklabels(('Class1','Class2','Class3'))
 ax.legend((rects1[0],rects2[0],rects3[0]),('Total','Survived','Not Survived'),loc='best')
@@ -71,3 +105,4 @@ autolabel(rects1)
 autolabel(rects2)
 autolabel(rects3)
 plt.show()
+
