ebff79502a0c9149c315442b736a16c87f65695d
Vaibhav <Vaibhavkamatrediffmail.com@gmail.com>
2017-08-14 01:08:52 +0530

    Add bar chart for people onboard based on port of embarkation

--- Titanic_Analysis.py
+++ Titanic_Analysis.py
@@ -32,17 +32,23 @@ def autolabel(rects,x=0.15,y=0.6):
     for rect in rects:
         h = rect.get_height()
         plt.text(rect.get_x()+x,y*h,int(h),ha='center',va='bottom',color='white',size=12)
-    
-data = pd.read_csv("titanic_data.csv")
-  
 
+
+
+
+
+
+
+
+    
+data = pd.read_csv("titanic_data.csv")# Input from the Titanic data CSV file
 # Age distribution within the passenger classes (Kernel Density Estimation)     
-ax = plt.subplot2grid((2,3),(0,0), colspan=3)
+ax1 = plt.subplot2grid((3,3),(0,0),colspan=3)
 data.Age[data.Pclass == 1].plot(kind='kde',color='blue')    
 data.Age[data.Pclass == 2].plot(kind='kde',color='red')
 data.Age[data.Pclass == 3].plot(kind='kde',color='orange')
 plt.xlabel("Age")    
-plt.title("Age Distribution (Kernel Density Estimation) Within Classes")
+plt.title("Age Distribution (Kernel Density Estimation) within the passenger classes",size=14)
 plt.legend(('1st Class', '2nd Class','3rd Class')) 
 plt.show()
 
@@ -61,25 +67,33 @@ width = 0.3
 index = np.arange(3)
 fig,ax = plt.subplots()
    
-ax = plt.subplot2grid((2,3),(1,0), colspan=3)   
-rects1 = ax.bar(index,females_onboard,width,color='blue')
-rects2 = ax.bar(index+width,males_onboard,width,color='green')      
-ax.set_xticks(index+width)
-ax.set_xticklabels(('Class1','Class2','Class3'))
-ax.legend((rects1[0],rects2[0]),('Females','Males'),loc='best')
-ax.set_ylim(0,400)
-plt.title("Number of males and females onboard the Titanic, grouped according to the passenger class")
-autolabel(rects1,0.15,0.2)
+ax2 = plt.subplot2grid((3,3),(1,0),colspan=3)   
+rects1 = ax2.bar(index,females_onboard,width,color='blue')
+rects2 = ax2.bar(index+width,males_onboard,width,color='green')      
+ax2.set_xticks(index+width)
+ax2.set_xticklabels(('Class1','Class2','Class3'))
+ax2.legend((rects1[0],rects2[0]),('Females','Males'),loc='best')
+ax2.set_ylim(0,400)
+plt.title("Number of males and females onboard the Titanic, grouped according to the passenger class",size=14)
+autolabel(rects1,0.15,0)
 autolabel(rects2,0.15,0.2)
 plt.show()
 
 
 
+# number of people based on port of embarkation
+data_by_embark = data.groupby('Embarked')
+embarked = np.array(data_by_embark.count()['Survived'])
 
-
-
-
-#############################################################################################
+ax3 = plt.subplot2grid((3,3),(2,0),colspan=3)
+rects = ax3.barh(index,embarked,width,color='brown')
+ax3.set_yticks(index+width)
+ax3.set_yticklabels(('Cherbourg','Queenstown','Southampton'))
+for i in range(len(embarked)):
+    plt.text(embarked[i],i,embarked[i],color='blue',size=12)
+plt.title("Number of people onboard Titanic based on port of Embarkation",size=14)
+plt.show()
+#########################################################################################################################
 
 # Number Of People And Those Survived, Grouped By The Passenger Class
 total_survived = (data['Survived']).sum()
